b307b908527edc421e45d91fd800f56cee4efb54
xinconan <xinconan2@sina.com>
2022-06-11 21:56:22 +0800

    :tada: init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <el-button>xinconan</el-button>
+</template>
+
+<style>
+html, body{
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
