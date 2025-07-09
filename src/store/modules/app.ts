42adbf44c59b23cbb6c328a57fc3a4514d2e3c46
xinconan <xinconan2@sina.com>
2025-07-09 16:03:09 +0800

    fix: pinia persist

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -16,7 +16,7 @@ export const useAppStore = defineStore('app', {
   },
   // 本地持久化
   persist: {
-    paths: ['user', 'chineseToken'],
+    pick: ['user', 'chineseToken'],
   },
   getters: {
     getCollapse(): boolean {
